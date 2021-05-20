0ea8459263369ece6ba64b7a14002e9f6809daa9
prasetioad <waghupd@gmail.com>
2021-05-20 21:58:11 +0700

    new merge and error handling

--- src/configs/redux/reducer/index.js
+++ src/configs/redux/reducer/index.js
@@ -1,16 +1,10 @@
 import {combineReducers} from 'redux'
 import User from './users'
 import Key from './key'
-<<<<<<< HEAD
-const RootReducer = combineReducers({
-    user : User,
-    key : Key
-=======
 import Chart from './chart'
 const RootReducer = combineReducers({
     user : User,
     key : Key,
     chart : Chart
->>>>>>> 7cf0668ab8c550fdfddd5f629fb88cff0cf23483
 })
 export default RootReducer
